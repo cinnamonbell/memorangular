e5f7755853fc4d80c14846ec1173c413aa4fb83f
Robert Xie <rxie96@gmail.com>
2020-04-20 10:29:33 -0400

    initial login form mockup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CardViewComponent } from './card-view/card-view.component';
 import { CollectionViewComponent } from './collection-view/collection-view.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardViewComponent,
-    CollectionViewComponent
+    CollectionViewComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule

899a6d9e68381354a5d895b63631f749f5d615c9
Robert Xie <rxie96@gmail.com>
2020-04-20 14:11:56 -0400

    home page and login page concept

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { CardViewComponent } from './card-view/card-view.component';
 import { CollectionViewComponent } from './collection-view/collection-view.component';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -13,10 +15,12 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     CardViewComponent,
     CollectionViewComponent,
     LoginComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
